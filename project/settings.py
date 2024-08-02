1a65a9908dd8f011414c80a4f8517dbecf0ab2cc
soka03 <soka27@naver.com>
2024-08-02 23:20:20 +0900

    cors

--- project/settings.py
+++ project/settings.py
@@ -59,6 +59,28 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+
 SITE_ID = 1
 
 MIDDLEWARE = [
