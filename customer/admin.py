b25eceb86ec2fd4a1db46f3dce8a07e108a9596a
soka03 <soka27@naver.com>
2024-08-04 11:48:10 +0900

    admin

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'pickup_time', 'body', 'order',  'order_date')
+    
+    
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'customer', 'rating', 'comment', 'created_at')
