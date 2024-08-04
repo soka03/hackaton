190d7e3e754879682d0b2535aa81a41ea273eff1
soka03 <soka27@naver.com>
2024-08-04 17:21:30 +0900

    permissions

--- boss/views.py
+++ boss/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 from .serializers import *
-from member.permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
