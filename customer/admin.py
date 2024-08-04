78770cc7398cc246d58e0f18a623eabdba5d15aa
soka03 <soka27@naver.com>
2024-08-04 11:55:07 +0900

    admin

--- customer/admin.py
+++ customer/admin.py
@@ -3,8 +3,15 @@ from .models import *
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'pickup_time', 'body', 'order',  'order_date')
-    
+    list_display = ('id', 'get_user', 'get_product', 'customer', 'pickup_time', 'body', 'order',  'order_date')
+
+    def get_user(self, obj):
+        return obj.post.user.username
+    get_user.short_description = 'User'
+
+    def get_product(self, obj):
+        return obj.post.product
+    get_product.short_description = 'Product'
     
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
