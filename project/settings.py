b8be16fe4d61d2aa17449178fdaee1626e8ac906
soka03 <soka27@naver.com>
2024-08-06 20:46:36 +0900

    a

--- project/settings.py
+++ project/settings.py
@@ -73,7 +73,7 @@ CORS_ORIGIN_WHITELIST =[
     "http://127.0.0.1:8000",
     "http://127.0.0.1:5173",
     "https://ourvege.store",
-    "https://main--ourvegestore123.netlify.app/",
+    "https://main--ourvegestore123.netlify.app",
 ]
 
 
